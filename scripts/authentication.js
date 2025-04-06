ae67594f5058cc58226777cfd5c299f1629b66fb
Glonk <jaspreetbath98@gmail.com>
2025-04-05 21:46:10 -0700

    Cleaned up some conflict errors. Added a profile that saves user name and city into database. Added the function to reset if Sunday has passed or IS Sunday date. Also resets currentCarbonScore when you login.

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -7,10 +7,34 @@ function getCurrentDate() {
 }
 
 //Function to set the current day of users time.
-function getCurrentDay() {
+function getDayOfWeek(date) {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const current = new Date();
-    return days[current.getDay()];
+    return days[date.getDay()];
+}
+
+//Function to set the current day
+function getCurrentDay() {
+    return getDayOfWeek(new Date());
+}
+
+//Function to check if it's a Sunday or if it has passed a Sunday
+function hasSundayPassed(lastSignInDate, currentDate) {
+    const lastDayIndex = lastSignInDate.getDay();
+    const currentDayIndex = currentDate.getDay();
+
+    if (lastDayIndex !== 0 && currentDayIndex !== 0 && lastDayIndex > currentDayIndex) {
+        return true;
+    }
+
+    //Same day = no change
+    if (lastDayIndex === currentDayIndex) {
+        return false;
+    }
+
+    if (currentDayIndex === 0) {
+        return true;
+    }
+    return false;
 }
 
 var uiConfig = {
@@ -24,14 +48,17 @@ var uiConfig = {
                 name: user.displayName,
                 email: user.email,
                 lastSignInDate: getCurrentDate(),
-                weeklyCarbonScore: "0000",
-                currentCarbonScore: "000",
-                totalCarbonScore: "0000",
+                weeklyCarbonScore: "0.000",
+                currentCarbonScore: "0.000",
+                totalCarbonScore: "0.000",
                 day: getCurrentDay()
             };
 
+            const userRef = db.collection("users").doc(user.uid);
+
             if (authResult.additionalUserInfo.isNewUser) {
-                db.collection("users").doc(user.uid).set(userData)
+                //Creates their new document for a new user
+                userRef.set(userData)
                     .then(() => {
                         console.log("New user added to Firestore");
                         window.location.assign("main.html");
@@ -40,13 +67,39 @@ var uiConfig = {
                         console.error("Error adding user: ", error);
                     });
             } else {
-                db.collection("users").doc(user.uid).update({
-                    email: user.email,
-                    lastSignInDate: getCurrentDate(),
-                    // weeklyCarbonScore: "0.00",
-                    // currentCarbonScore: "0.00",
-                    // totalCarbonScore: "0.00",
-                    day: getCurrentDay()
+                //Checks if we need to reset weeklyCarbonScore and currentScore
+                db.runTransaction(transaction => {
+                    return transaction.get(userRef).then(doc => {
+                        if (!doc.exists) {
+                            //If the document doesn't exist, create it
+                            transaction.set(userRef, userData);
+                            return;
+                        }
+
+                        const data = doc.data();
+                        const lastSignInDate = data.lastSignInDate ? data.lastSignInDate.toDate() : new Date();
+                        const currentDate = new Date();
+                        const today = getCurrentDay();
+                        let weeklyCarbonScore = parseFloat(data.weeklyCarbonScore) || 0;
+
+                        //Resetting the weekly carbonscore if it's a Sunday or has passed a Sunday
+                        if (today === "Sunday" || hasSundayPassed(lastSignInDate, currentDate)) {
+                            weeklyCarbonScore = 0;
+                            console.log("Resetting weeklyCarbonScore because it's Sunday or a Sunday has passed!");
+                        }
+
+                        //Resetting currentScore every sign in.
+                        const currentCarbonScore = 0; //Since sunday is reset, 0 is a sunday.
+
+                        // Updating the user's document
+                        transaction.update(userRef, {
+                            email: user.email,
+                            lastSignInDate: getCurrentDate(),
+                            day: today,
+                            weeklyCarbonScore: weeklyCarbonScore.toFixed(3),
+                            currentCarbonScore: currentCarbonScore.toFixed(3)
+                        });
+                    });
                 })
                     .then(() => {
                         console.log("Existing user data updated");
@@ -54,15 +107,6 @@ var uiConfig = {
                     })
                     .catch((error) => {
                         console.error("Error updating existing user: ", error);
-
-                        //Create doc if use doesn't exist
-                        if (error.code === "not-found") {
-                            db.collection("users").doc(user.uid).set(userData)
-                                .then(() => {
-                                    console.log("Created document for existing user");
-                                    window.location.assign("main.html");
-                                });
-                        }
                     });
             }
             return false;
--- scripts/map.js
+++ scripts/map.js
@@ -3,7 +3,6 @@ const map = new mapboxgl.Map({
   container: 'map',
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: 'mapbox://styles/mapbox/streets-v11',
-
   center: [-123., 49.24],
   zoom: 13
 });
@@ -21,101 +20,55 @@ const geocoder = new MapboxGeocoder({
 const start = [-123, 49.24];
 // create a function to make a directions request
 async function getRoute(end) {
-  const query = await fetch(
-    `https://api.mapbox.com/directions/v5/mapbox/driving-traffic/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
-    { method: 'GET' },
-
-  );
-  const json = await query.json();
-  const data = json.routes[0];
-  const route = data.geometry.coordinates;
-  const geojson = {
-    type: 'Feature',
-    properties: {},
-    geometry: {
-      type: 'LineString',
-      coordinates: route
-    }
-  };
-  // if the route already exists on the map, we'll reset it using setData
-  if (map.getSource('route')) {
-    map.getSource('route').setData(geojson);
-  }
-  // otherwise, we'll make a new request
-  else {
-    map.addLayer({
-      id: 'route',
-      type: 'line',
-      source: {
-        type: 'geojson',
-        data: geojson
-      },
-      layout: {
-        'line-join': 'round',
-        'line-cap': 'round'
-      },
-      paint: {
-        'line-color': '#3887be',
-        'line-width': 5,
-        'line-opacity': 0.75
+    const query = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/driving-traffic/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      { method: 'GET' }
+    );
+    const json = await query.json();
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+    const geojson = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: route
       }
-    });
-  }
-}
-
-map.on('load', () => {
-  // make an initial directions request that
-  // starts and ends at the same location
-  getRoute(start);
-
-  // Add starting point to the map
-  map.addLayer({
-    id: 'point',
-    type: 'circle',
-    source: {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'Point',
-              coordinates: start
-            }
-          }
-        ]
-      }
-    },
-    paint: {
-      'circle-radius': 10,
-      'circle-color': '#3887be'
+    };
+    // if the route already exists on the map, we'll reset it using setData
+    if (map.getSource('route')) {
+      map.getSource('route').setData(geojson);
     }
-  });
-});
-
-map.on('click', (event) => {
-
-  const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
-  const end = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'Point',
-          coordinates: coords
+    // otherwise, we'll make a new request
+    else {
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: geojson
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75
         }
-      }
-    ]
-  };
-  if (map.getLayer('end')) {
-    map.getSource('end').setData(end);
-  } else {
+      });
+    }
+  }
+  
+  map.on('load', () => {
+    // make an initial directions request that
+    // starts and ends at the same location
+    getRoute(start);
+  
+    // Add starting point to the map
     map.addLayer({
-      id: 'end',
+      id: 'point',
       type: 'circle',
       source: {
         type: 'geojson',
@@ -127,7 +80,7 @@ map.on('click', (event) => {
               properties: {},
               geometry: {
                 type: 'Point',
-                coordinates: coords
+                coordinates: start
               }
             }
           ]
@@ -135,12 +88,56 @@ map.on('click', (event) => {
       },
       paint: {
         'circle-radius': 10,
-        'circle-color': '#f30'
+        'circle-color': '#3887be'
       }
     });
-  }
-  getRoute(coords);
-});
+  });
+
+  map.on('click', (event) => {
+    const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+    const end = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'Point',
+            coordinates: coords
+          }
+        }
+      ]
+    };
+    if (map.getLayer('end')) {
+      map.getSource('end').setData(end);
+    } else {
+      map.addLayer({
+        id: 'end',
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                  type: 'Point',
+                  coordinates: coords
+                }
+              }
+            ]
+          }
+        },
+        paint: {
+          'circle-radius': 10,
+          'circle-color': '#f30'
+        }
+      });
+    }
+    getRoute(coords);
+  });
 map.addControl(geocoder);
 
 
